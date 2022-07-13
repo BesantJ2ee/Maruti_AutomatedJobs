39ec1b5981df4910d027e11d80cfc72bbb007988
Gamma <simonjustin321@gmail.com>
2022-07-13 17:04:46 +0530

    saved

---
+++ src/main/java/com/Maruti/Jobs/MarutiJobsApplication.java
@@ -0,0 +1,13 @@
+package com.Maruti.Jobs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarutiJobsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarutiJobsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Maruti/Jobs/MarutiJobsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Maruti.Jobs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarutiJobsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
